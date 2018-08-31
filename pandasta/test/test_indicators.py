b713ebb8cd11cd4c20b6a8e19868f2f0138fdc77
Preslav Rachev <rachev.preslav@gmail.com>
2018-08-31 07:02:04 +0200

    Adds a simple unit test

---
+++ pandasta/test/test_indicators.py
@@ -0,0 +1,44 @@
+import random
+import unittest
+
+from pandasta.indicators import BacktestingTaDataFrame, Order
+from pandasta.indicators import TradingStrategy
+
+MIN_AMOUNT = 0.001
+
+
+class TestStrategy(TradingStrategy):
+
+    def __init__(self, min_amount: float = MIN_AMOUNT) -> None:
+        super().__init__()
+        self.min_amount = min_amount
+
+    def generate_order(self, record, funds, balance) -> Order:
+        return Order(Order.Decision.BUY, self.min_amount)
+
+
+class TestPandasTA(unittest.TestCase):
+
+    def test_backtesting_when_all_orders_ask_for_sub_minimum_amounts(self):
+        data = [{
+            "time": i,
+            "low": max(i + random.randint(-100, -50), 1),
+            "high": max(i + random.randint(50, 100), 1),
+            "open": max(i + random.randint(-50, 50), 1),
+            "close": max(i + random.randint(-50, 50), 1)
+        } for i in range(1000)]
+
+        df = BacktestingTaDataFrame(data,
+                                    funds=1000,
+                                    min_amount=MIN_AMOUNT,
+                                    indicators=[])
+        strategy = TestStrategy(min_amount=MIN_AMOUNT - 0.0001)
+        res = df.apply_strategy(strategy)
+
+        all_rejected = (res['statuses'] == 'REJECTED').all()
+
+        self.assertTrue(all_rejected, 'Not all orders seem to have been rejected. Please, re-check the logic')
+
+
+if __name__ == '__main__':
+    unittest.main()
