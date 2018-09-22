2b7b3c69700c8c0bed11f4de43f7f69daa5b5a96
Preslav Rachev <rachev.preslav@gmail.com>
2018-09-22 06:38:51 +0200

    Adds requirements to the setup file

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from distutils.core import setup
 setup(name='pandas-ta',
       version='0.1',
-      packages=['pandasta'],
-      )
+      install_requires=[
+          'dataclasses',
+          'pandas',
+          'pyfinance'
+      ],
+      packages=['pandasta'])
