1a6841edf5233f758036acfb98adaeb66ab92db6
Preslav Rachev <rachev.preslav@gmail.com>
2019-10-25 13:15:39 +0200

    Pin dependency versions

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from distutils.core import setup
 setup(
     name="pandas-ta",
     version="0.1",
-    install_requires=["dataclasses==0.6", "pandas==0.20.3", "pyfinance==1.1.1"],
+    install_requires=[
+        "dataclasses==0.6",
+        "pandas==0.20.3",
+        "pandas-datareader==0.7.0",
+        "pyfinance==1.1.1",
+    ],
     packages=["pandasta"],
 )
