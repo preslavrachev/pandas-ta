e2b026574866e623c5f2de0bdb7b853c717c86e7
Preslav Rachev <rachev.preslav@gmail.com>
2020-03-22 15:35:10 +0100

    Pin library versions

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
         "dataclasses==0.6",
         "pandas==0.20.3",
         "pandas-datareader==0.7.0",
+        "seaborn==0.9.0",
+        "statsmodels==0.10.1",
         "pyfinance==1.1.1",
     ],
     packages=["pandasta"],
